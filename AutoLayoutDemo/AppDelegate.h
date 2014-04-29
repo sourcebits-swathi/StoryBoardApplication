49016f76a39365139e6fac765bbe16cbda833f3f
Swathi Tata <swatitata@Swathi-Tatas-iMac.local>
2014-04-29 11:12:35 +0530

    Initial Commit

---
+++ AutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDemo
+//
+//  Created by Swathi Tata on 29/04/14.
+//  Copyright (c) 2014 Swathi Tata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
