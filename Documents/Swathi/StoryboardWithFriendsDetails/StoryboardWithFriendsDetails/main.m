3c1026464d90e58adfb539298cd53fa6c830da8b
Sourcebits <swathi.tata@sourcebits.com>
2014-05-08 17:31:32 +0530

    first commit

---
+++ Documents/Swathi/StoryboardWithFriendsDetails/StoryboardWithFriendsDetails/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryboardWithFriendsDetails
+//
+//  Created by Swathi Tata on 08/05/14.
+//  Copyright (c) 2014 Swathi Tata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Documents/Swathi/StoryboardWithFriendsDetails/StoryboardWithFriendsDetailsTests/StoryboardWithFriendsDetailsTests.m
@@ -0,0 +1,34 @@
+//
+//  StoryboardWithFriendsDetailsTests.m
+//  StoryboardWithFriendsDetailsTests
+//
+//  Created by Swathi Tata on 08/05/14.
+//  Copyright (c) 2014 Swathi Tata. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StoryboardWithFriendsDetailsTests : XCTestCase
+
+@end
+
+@implementation StoryboardWithFriendsDetailsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
